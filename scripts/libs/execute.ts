3bf4837bd3350799ccaa0e1807e99997ed767dc1
Thada Wangthammang <thada.wth@gmail.com>
2024-01-03 22:35:34 +0700

    add basic test runner

--- scripts/libs/execute.ts
+++ scripts/libs/execute.ts
@@ -1,7 +1,7 @@
 import { execa, Options as ExecaOptions } from 'execa';
 
-export async function execute(process: string, args: string[], option: ExecaOptions & { dryRun?: boolean }) {
-  const dryRun = option.dryRun ?? false;
+export async function execute(process: string, args: string[], option?: ExecaOptions & { dryRun?: boolean }) {
+  const dryRun = option?.dryRun ?? false;
   if (dryRun) {
     console.log(`[dry-run] ${process} ${args.join(' ')}`);
     return;
