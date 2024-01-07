cd64d90f1875631109ac7ec187ed9a21943eaca1
Thada Wangthammang <thada.wth@gmail.com>
2024-01-07 08:16:38 +0700

    use vitest for test runner

--- packages/inversify/__tests__/resolve.test.ts
+++ packages/inversify/__tests__/resolve.test.ts
@@ -1,16 +1,21 @@
 import "reflect-metadata"
+import { expect, test } from 'vitest'
 import { injector } from '../src/main';
+import { container } from './helpers/container';
 import { PostService } from './helpers/post.service';
-import { container } from "./helpers/container";
 
-// ------------------------------------------------------------- 
-// Arrange
+test('resolve', async () => {
+  const services = injector(container)
+    .inject('postService', PostService).to<PostService>()
+    .resolve();
 
-// Act
-const services = injector(container)
-  .inject('postService', PostService).to<PostService>()
-  .resolve();
-// Assert
-if (services.postService.getPost() !== 'Post: DataService') {
-  throw new Error('Test failed');
-}
+  expect(services.postService.getPost()).toBe("Post: DataService")
+})
+
+test('resolve with resolver', async () => {
+  const result = injector(container)
+    .inject('postService', PostService).to<PostService>()
+    .resolver(({ postService }) => postService.getPost());
+
+  expect(result).toBe("Post: DataService");
+});
